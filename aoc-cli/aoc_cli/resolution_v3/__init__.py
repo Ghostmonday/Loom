44e8b27fce61eeb3091865952ff0e60871222692
Ghostmonday <ghostmonday@gmail.com>
2026-07-01 00:07:48 -0700

    Add resolution proposal data models

--- aoc-cli/aoc_cli/resolution_v3/__init__.py
+++ aoc-cli/aoc_cli/resolution_v3/__init__.py
@@ -13,6 +13,14 @@ from aoc_cli.resolution_v3.model import (
     Provenance,
     Status,
 )
+from aoc_cli.resolution_v3.proposal import (
+    ProposalDecision,
+    ProposalDecisionStatus,
+    ProposalKind,
+    ResolutionProposal,
+    canonicalize_proposals,
+    reject_proposal,
+)
 from aoc_cli.resolution_v3.scc import tarjan_scc
 from aoc_cli.resolution_v3.worklist import NORMAL, URGENT, Worklist
 
@@ -28,9 +36,15 @@ __all__ = [
     "LocusKind",
     "Modality",
     "Node",
+    "ProposalDecision",
+    "ProposalDecisionStatus",
+    "ProposalKind",
     "Provenance",
+    "ResolutionProposal",
     "Status",
     "Worklist",
+    "canonicalize_proposals",
+    "reject_proposal",
     "resolve",
     "tarjan_scc",
 ]
